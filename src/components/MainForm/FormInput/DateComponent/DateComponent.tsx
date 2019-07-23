0ba5dc7498efcda4fc43dca6476043973807debf
Piotr Morawski <piotrmorawski90@gmail.com>
2019-07-23 21:36:07 +0200

    MR dateComponent refactor

--- src/components/MainForm/FormInput/DateComponent/DateComponent.tsx
+++ src/components/MainForm/FormInput/DateComponent/DateComponent.tsx
@@ -10,33 +10,36 @@ export enum TimeFormat {
   PM = "PM"
 }
 
-export const transformDate = (date: string): Date => new Date(date+ ":00Z")
+export const transformDate = (date: string): Date => new Date(date + ":00Z")
 
 export const DateComponent = ({
   input,
   meta: { active, error, touched },
   label
 }: any): ReactElement => {
-
   const [day, setDay] = useState<string>("")
   const [time, setTime] = useState<string>("")
   const [format, setFormat] = useState<TimeFormat>(TimeFormat.AM)
 
-
-  const handleDateChange = event => {
-    setDay(event.target.value)
-    const date = `${event.target.value}T${time}`
-    time && input.onChange(date)
+  const handleDateChange = ({target: {value}}) => {
+    setDay(value)
+    if (time) {
+      const date = `${value}T${timeConvertor(time, format)}`
+      input.onChange(date)
+    }
   }
   const handleTimeChange = value => {
     setTime(value)
-    const date = `${day}T${timeConvertor(value, format)}`
-    day && input.onChange(date)
-
+    if (day) {
+      const date = `${day}T${timeConvertor(value, format)}`
+      input.onChange(date)
+    }
   }
-  const handleFormatChange = event => {
-    setFormat(event.target.value);
-    (day && time)  && input.onChange(`${day}T${timeConvertor(time, event.target.value)}`)
+  const handleFormatChange = ({target: {value}}) => {
+    setFormat(value)
+    if (day && time) {
+      input.onChange(`${day}T${timeConvertor(time, value)}`)
+    }
   }
 
   return (
@@ -50,7 +53,11 @@ export const DateComponent = ({
         className="DateComponent-dateInput"
       />
       <span>at</span>
-      <TimeInput onTimeChange={handleTimeChange} placeholder={"--:--"} input={input} />
+      <TimeInput
+        onTimeChange={handleTimeChange}
+        placeholder={"--:--"}
+        input={input}
+      />
       <input
         {...input}
         type="radio"
