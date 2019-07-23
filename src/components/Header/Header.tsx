0b0a86d6c80827edd10a9297d47458086896a8cf
Piotr Morawski <piotrmorawski90@gmail.com>
2019-07-23 22:22:34 +0200

    add Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <div className="Header-Stripe" />
+      <div className="Header-Title">
+        <span className="Header-Title--text"> New event</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
