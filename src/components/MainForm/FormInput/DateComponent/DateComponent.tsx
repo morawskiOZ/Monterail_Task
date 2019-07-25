be199bc27fb646109c3694cf319eaa2b6887ca86
Piotr Morawski <piotrmorawski90@gmail.com>
2019-07-25 21:05:51 +0200

    style data component

--- src/components/MainForm/FormInput/DateComponent/DateComponent.tsx
+++ src/components/MainForm/FormInput/DateComponent/DateComponent.tsx
@@ -1,9 +1,10 @@
 import { timeConvertor } from "helpers/components/MainForm/timeConvertor"
-import React, { useState, ReactElement } from "react"
+import React, { ReactElement, useState } from "react"
 import "./DateComponent.scss"
 import TimeInput from "./TimeInput/TimeInput"
 
 // TODO: add types everywhere
+// TODO: make this component more generic, accepting styles from outside for example, more generic props
 
 export enum TimeFormat {
   AM = "AM",
@@ -21,7 +22,7 @@ export const DateComponent = ({
   const [time, setTime] = useState<string>("")
   const [format, setFormat] = useState<TimeFormat>(TimeFormat.AM)
 
-  const handleDateChange = ({target: {value}}) => {
+  const handleDateChange = ({ target: { value } }) => {
     setDay(value)
     if (time) {
       const date = `${value}T${timeConvertor(time, format)}`
@@ -35,7 +36,7 @@ export const DateComponent = ({
       input.onChange(date)
     }
   }
-  const handleFormatChange = ({target: {value}}) => {
+  const handleFormatChange = ({ target: { value } }) => {
     setFormat(value)
     if (day && time) {
       input.onChange(`${day}T${timeConvertor(time, value)}`)
@@ -43,38 +44,53 @@ export const DateComponent = ({
   }
 
   return (
-    <div tabIndex={0} className={""}>
-      <label>{label}</label>
-      <input
-        {...input}
-        type="date"
-        onChange={handleDateChange}
-        value={day}
-        className="DateComponent-dateInput"
-      />
-      <span>at</span>
-      <TimeInput
-        onTimeChange={handleTimeChange}
-        placeholder={"--:--"}
-        input={input}
-      />
-      <input
-        {...input}
-        type="radio"
-        name="format"
-        value={TimeFormat.AM}
-        checked={format === TimeFormat.AM}
-        onChange={handleFormatChange}
-        required
-      />
-      <input
-        {...input}
-        type="radio"
-        name="format"
-        value={TimeFormat.PM}
-        checked={format === TimeFormat.PM}
-        onChange={handleFormatChange}
-      />
+    <div className="FormInput">
+      <label className="FormInput--column FormInput--firstColumn FormInput-label">
+        {label}
+      </label>
+
+      <div className="FormInput--column FormInput--secondColumn">
+        <div className="FormInput-multiElementRow">
+          <input
+            {...input}
+            type="date"
+            onChange={handleDateChange}
+            value={day}
+            className={`FormInput-input ${
+              day ? "" : "FormInput-input--placeholder"
+            }`}
+          />
+          <span>at</span>
+          <TimeInput
+            onTimeChange={handleTimeChange}
+            placeholder={"--:--"}
+            input={input}
+            className={"FormInput-input FormInput-input--verySmall"}
+          />
+          <input
+            {...input}
+            type="radio"
+            name="format"
+            value={TimeFormat.AM}
+            checked={format === TimeFormat.AM}
+            onChange={handleFormatChange}
+            required
+            className="FormInput-input FormInput-input--radio"
+          />
+          <span className="FormInput-description"> AM </span>
+          <input
+            {...input}
+            type="radio"
+            name="format"
+            value={TimeFormat.PM}
+            checked={format === TimeFormat.PM}
+            onChange={handleFormatChange}
+            className="FormInput-input FormInput-input--radio"
+          />
+          <span className="FormInput-description"> PM</span>
+        </div>
+      </div>
+
       {error && touched && <span>{error}</span>}
     </div>
   )
